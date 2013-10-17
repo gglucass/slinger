eca5f56536db10d0a54e11ec075cb5d2aa9784d4
Garm Lucassen <garm@frapp.it>
2013-10-17 18:26:41 +0200

    parse all groups

--- slinger.coffee.js
+++ slinger.coffee.js
@@ -1,3 +1,7 @@
+App = {}
+App.scrape_results = ''
+App.members_length = 0
+App.counter = 0
 document.addEventListener "DOMContentLoaded", ->
   chrome.tabs.query active: true, currentWindow: true, (tabs) ->
     type = undefined
@@ -9,7 +13,7 @@ document.addEventListener "DOMContentLoaded", ->
   
   
 
-initializeProfiles = (type, url) ->
+initializeProfiles = (type, url) ->  
   switch type
     when "group"
       new_url = 'http://www.linkedin.com/groups?viewMembers=&gid=' + url.split('gid=')[1].split('&')[0];
@@ -23,16 +27,30 @@ visitGroupPage = (url) ->
     type: "GET"
     url: url
     success: (response) ->
-      members = $(response).find('#content').find('.member')
+      members = $(response).find('#content').find('.member')      
+      App.members_length = members.length
+      App.counter = 0
       linkedin_ids = members.map (x) ->
         members[x].getAttribute("id").split("-")[1]
-      getProfile linkedin_id for linkedin_id in linkedin_ids
-      next_page = $(response).find('.paginate').find('a').find('strong')
-      new_next_page = next_page[1]
-      new_next_page = next_page[0] unless next_page[1]
-      if new_next_page.innerText.charCodeAt(0) != '171'
-        new_url = 'http://www.linkedin.com/' + $(new_next_page).parent()[0].getAttribute('href')
-        visitGroupPage(new_url)
+      getProfile linkedin_id, response for linkedin_id in linkedin_ids
+
+getNextPage = (response) ->
+  next_page = $(response).find('.paginate').find('a').find('strong')
+  new_next_page = next_page[1]
+  new_next_page = next_page[0] unless next_page[1]  
+  if (new_next_page == undefined || new_next_page.innerText.charCodeAt(0) == parseInt('171'))
+    derp = new Blob([App.scrape_results],
+      type: "text/plain"
+    )
+    a = document.createElement("a")
+    a.href = window.URL.createObjectURL(derp)
+    a.download = "results.html"
+    a.textContent = "Download file!"
+    document.body.appendChild a
+  else
+    new_url = 'http://www.linkedin.com/' + $(new_next_page).parent()[0].getAttribute('href')
+    visitGroupPage(new_url)
+        
       
       
 
@@ -40,23 +58,49 @@ visitCompanyPage = (url) ->
   console.log url
 
 
-getProfile = (linkedin_id) ->
+getProfile = (linkedin_id, memberpage) ->
   $.ajax
     type: "GET"
     url: "http://www.linkedin.com/profile/view?noScript=1&id=#{linkedin_id}"
     success: (response) ->
-      logConsole(response, linkedin_id)
+      logConsole(response, linkedin_id, memberpage)
 
-logConsole = (response, linkedin_id) ->
+logConsole = (response, linkedin_id, memberpage) ->
   member = $(response).find("#member-#{linkedin_id}")
-  given_name = $(member).find('span.given-name')[0].innerText;
-  family_name = $(member).find('span.family-name')[0].innerText;
-  professional_title = $(member).find('p.title')[0].innerText;
-  $('#profiles').append "<h2 id='profile-#{given_name}'> #{given_name} #{family_name} - #{professional_title}</h2><div id='positions-#{linkedin_id}'><h3>Positions:</h3></div>"
-  positions = $(response).find('.position')
+  given_name = $(member).find('span.given-name')[0].innerText
+  family_name = $(member).find('span.family-name')[0].innerText
+  professional_title = $(member).find('p.title')[0].innerText
+  profile_pic = ( $(response).find('#profile-picture').find('img').attr('src') || 'http://s.c.lnkd.licdn.com/scds/common/u/images/themes/katy/ghosts/person/ghost_person_100x100_v1.png')
+  App.scrape_results = App.scrape_results + "<h2 id='profile-#{given_name}'> #{given_name} #{family_name} - #{professional_title}</h2>
+  <a href='#{profile_pic}'><img src='#{profile_pic}'></a>
+  <div id='positions-#{linkedin_id}'><h3>Positions:</h3>"
+  positions = $(response).find('.position.experience')
   for position in positions
     title = $(position).find('strong.title').find('a')[0].innerText
     company = ( $(position).find('.org.summary')[0] || $(position).find('h4').find('a')[0] ).innerText
-    $("#positions-#{linkedin_id}").append "<p><b>#{title}</b> - <i>#{company}</i></p>"
-
-    document.body.match(/\bprofile\b,\bview?id=\b/)
+    company_profile = $(position).find('company-profile')    
+    // $(company_profile).hover ->
+    //   mini_profile = $(this).find('.minipanel-content.company-miniprofile')[0]
+    //   hq = $(mini_profile).find("abbr[title='Headquarters']").parent().parent().find('td').text()
+    //   logo = $(mini_profile).find('#logo-large-img').attr('src')
+    start =  ( $(position).find('.dtstart')[0] || 'undefined' ).innerText
+    end = ( $(position).find('.dtstamp')[0] || $(position).find('.dtend')[0] || 'undefined').innerText
+    App.scrape_results = App.scrape_results + "<p><b>#{title}</b> - <i>#{company}</i></p>
+    <p><b>Start: </b> #{start}</p>
+    <p><b>End: </b> #{end}</p>"
+  App.scrape_results = App.scrape_results + "</div><h3>Skills:</h3><p>"
+  skills = $(response).find('.endorse-item-name-text')
+  for skill in skills
+    App.scrape_results = App.scrape_results + "#{skill.innerText}, "
+  App.scrape_results = App.scrape_results + "</p><h3>Education:</h3>"
+  educations = $(response).find('.position.education')
+  for education in educations
+    title = $(education).find('h4.details-education')
+    school = $(education).find('a.school-link')[0].innerText
+    start = ( $(education).find('.dtstart')[0] || 'undefined').innerText
+    end = ( $(education).find('.dtstamp')[0] || $(education).find('.dtend')[0] || 'undefined' ).innerText
+    App.scrape_results = App.scrape_results + "<p><b>#{title[0].innerHTML}</b> - <i>#{school}</i></p>
+    <p><b>Start: </b> #{start}</p>
+    <p><b>End: </b> #{end}</p>"
+  App.scrape_results = App.scrape_results + "</div>"
+  getNextPage(memberpage) if ++App.counter is App.members_length
--- slinger.js
+++ slinger.js
@@ -1,4 +1,12 @@
-var getProfile, initializeProfiles, logConsole, visitCompanyPage, visitGroupPage;
+var App, getNextPage, getProfile, initializeProfiles, logConsole, visitCompanyPage, visitGroupPage;
+
+App = {};
+
+App.scrape_results = '';
+
+App.members_length = 0;
+
+App.counter = 0;
 
 document.addEventListener("DOMContentLoaded", function() {
   return chrome.tabs.query({
@@ -36,57 +44,95 @@ visitGroupPage = function(url) {
     type: "GET",
     url: url,
     success: function(response) {
-      var linkedin_id, linkedin_ids, members, new_next_page, new_url, next_page, _i, _len;
+      var linkedin_id, linkedin_ids, members, _i, _len, _results;
       members = $(response).find('#content').find('.member');
+      App.members_length = members.length;
+      App.counter = 0;
       linkedin_ids = members.map(function(x) {
         return members[x].getAttribute("id").split("-")[1];
       });
+      _results = [];
       for (_i = 0, _len = linkedin_ids.length; _i < _len; _i++) {
         linkedin_id = linkedin_ids[_i];
-        getProfile(linkedin_id);
+        _results.push(getProfile(linkedin_id, response));
       }
-      next_page = $(response).find('.paginate').find('a').find('strong');
-      new_next_page = next_page[1];
-      if (!next_page[1]) {
-        new_next_page = next_page[0];
-      }
-      if (new_next_page.innerText.charCodeAt(0) !== '171') {
-        new_url = 'http://www.linkedin.com/' + $(new_next_page).parent()[0].getAttribute('href');
-        return visitGroupPage(new_url);
-      }
+      return _results;
     }
   });
+};
+
+getNextPage = function(response) {
+  var a, derp, new_next_page, new_url, next_page;
+  next_page = $(response).find('.paginate').find('a').find('strong');
+  new_next_page = next_page[1];
+  if (!next_page[1]) {
+    new_next_page = next_page[0];
+  }
+  console.log(new_next_page.innerText.charCodeAt(0) == '171')
+  if (new_next_page === void 0 || new_next_page.innerText.charCodeAt(0) === parseInt('171')) {
+    derp = new Blob([App.scrape_results], {
+      type: "text/plain"
+    });
+    a = document.createElement("a");
+    a.href = window.URL.createObjectURL(derp);
+    a.download = "results.html";
+    a.textContent = "Download file!";
+    return document.body.appendChild(a);
+  } else {
+    new_url = 'http://www.linkedin.com/' + $(new_next_page).parent()[0].getAttribute('href');
+    return visitGroupPage(new_url);
+  }
 };
 
 visitCompanyPage = function(url) {
   return console.log(url);
 };
 
-getProfile = function(linkedin_id) {
+getProfile = function(linkedin_id, memberpage) {
   return $.ajax({
     type: "GET",
     url: "http://www.linkedin.com/profile/view?noScript=1&id=" + linkedin_id,
     success: function(response) {
-      return logConsole(response, linkedin_id);
+      return logConsole(response, linkedin_id, memberpage);
     }
   });
 };
 
-logConsole = function(response, linkedin_id) {
-  var company, family_name, given_name, member, position, positions, professional_title, title, _i, _len, _results;
+logConsole = function(response, linkedin_id, memberpage) {
+  var company, company_profile, education, educations, end, family_name, given_name, member, position, positions, professional_title, profile_pic, school, skill, skills, start, title, _i, _j, _k, _len, _len1, _len2;
   member = $(response).find("#member-" + linkedin_id);
   given_name = $(member).find('span.given-name')[0].innerText;
   family_name = $(member).find('span.family-name')[0].innerText;
   professional_title = $(member).find('p.title')[0].innerText;
-  $('#profiles').append("<h2 id='profile-" + given_name + "'> " + given_name + " " + family_name + " - " + professional_title + "</h2><div id='positions-" + linkedin_id + "'><h3>Positions:</h3></div>");
-  positions = $(response).find('.position');
-  _results = [];
+  profile_pic = $(response).find('#profile-picture').find('img').attr('src') || 'http://s.c.lnkd.licdn.com/scds/common/u/images/themes/katy/ghosts/person/ghost_person_100x100_v1.png';
+  App.scrape_results = App.scrape_results + ("<h2 id='profile-" + given_name + "'> " + given_name + " " + family_name + " - " + professional_title + "</h2>  <a href='" + profile_pic + "'><img src='" + profile_pic + "'></a>  <div id='positions-" + linkedin_id + "'><h3>Positions:</h3>");
+  positions = $(response).find('.position.experience');
   for (_i = 0, _len = positions.length; _i < _len; _i++) {
     position = positions[_i];
     title = $(position).find('strong.title').find('a')[0].innerText;
     company = ($(position).find('.org.summary')[0] || $(position).find('h4').find('a')[0]).innerText;
-    $("#positions-" + linkedin_id).append("<p><b>" + title + "</b> - <i>" + company + "</i></p>");
-    _results.push(document.body.match(/\bprofile\b,\bview?id=\b/));
+    company_profile = $(position).find('company-profile');
+    end = ($(position).find('.dtstamp')[0] || $(position).find('.dtend')[0] || 'undefined').innerText;
+    App.scrape_results = App.scrape_results + ("<p><b>" + title + "</b> - <i>" + company + "</i></p>    <p><b>Start: </b> " + start + "</p>    <p><b>End: </b> " + end + "</p>");
   }
-  return _results;
+  App.scrape_results = App.scrape_results + "</div><h3>Skills:</h3><p>";
+  skills = $(response).find('.endorse-item-name-text');
+  for (_j = 0, _len1 = skills.length; _j < _len1; _j++) {
+    skill = skills[_j];
+    App.scrape_results = App.scrape_results + ("" + skill.innerText + ", ");
+  }
+  App.scrape_results = App.scrape_results + "</p><h3>Education:</h3>";
+  educations = $(response).find('.position.education');
+  for (_k = 0, _len2 = educations.length; _k < _len2; _k++) {
+    education = educations[_k];
+    title = $(education).find('h4.details-education');
+    school = $(education).find('a.school-link')[0].innerText;
+    start = ($(education).find('.dtstart')[0] || 'undefined').innerText;
+    end = ($(education).find('.dtstamp')[0] || $(education).find('.dtend')[0] || 'undefined').innerText;
+    App.scrape_results = App.scrape_results + ("<p><b>" + title[0].innerHTML + "</b> - <i>" + school + "</i></p>    <p><b>Start: </b> " + start + "</p>    <p><b>End: </b> " + end + "</p>");
+  }
+  App.scrape_results = App.scrape_results + "</div>";
+  if (++App.counter === App.members_length) {
+    return getNextPage(memberpage);
+  }
 };
